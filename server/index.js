4958f06bdc339784c57062392c3286ef827d35ff
Abhishek Kushwaha <kabhishek5710@gmail.com>
2024-05-02 13:10:46 +0530

    Set up the server side

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
+});
+
+app.get('/', (req, res) => {
+    res.send("")
+})
